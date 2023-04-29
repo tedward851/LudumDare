41c45eee93cb861ad1fd5be13b63466d617c97e3
tedward851 <96402421+tedward851@users.noreply.github.com>
2023-04-29 12:45:13 -0400

    Add new dog art

--- lib/dog.gd
+++ lib/dog.gd
@@ -16,12 +16,16 @@ func _process(delta):
 	var velocity = Vector2.ZERO # The player's movement vector.
 	if Input.is_action_pressed("move_right"):
 		velocity.x += 1
+		rotation = PI / 2
 	if Input.is_action_pressed("move_left"):
 		velocity.x -= 1
+		rotation = 3 * PI / 2
 	if Input.is_action_pressed("move_down"):
 		velocity.y += 1
+		rotation = PI
 	if Input.is_action_pressed("move_up"):
 		velocity.y -= 1
+		rotation = 0
 
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
