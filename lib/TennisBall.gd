ac96a844e63516966ecc79a3d02caaef829c4f7c
Silas Smith <silas730@gmail.com>
2023-04-29 09:00:57 -0400

    Better person and ball

--- lib/TennisBall.gd
+++ lib/TennisBall.gd
@@ -1,22 +1,50 @@
 extends Area2D
 
-var speed = 200
+var speed = 100
 var velocity = Vector2()
-var friction = Vector2(-1,1)
+var frictionCoef = 1
+var timer
+var isSlowing = false
+var carrier = Node2D
+var isCarried = false
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	setVelocity(1,1)
+	timer = createTimer(1)
+	timer.timeout.connect(func(): isSlowing = true)
+	add_child(timer)
+	timer.start()
+	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	velocity += friction * delta
-	position += velocity * delta
+	var frictionVector = velocity.rotated(PI).normalized() * frictionCoef
+	if isSlowing:
+		velocity += frictionVector * delta
+	
+	if !isCarried:
+		position += velocity * delta * speed
+	else:
+		position = carrier.position
+		
 	
 func setVelocity(x, y):
 	velocity.x = x
 	velocity.y = y
-	velocity = velocity.normalized() * speed
+	velocity = velocity.normalized()
 	
 func setSpeed(v):
 	speed = v
+
+func createTimer(time):
+	timer = Timer.new()
+	timer.wait_time = time
+	timer.one_shot = true
+	return timer
+
+func attachToNode(node):
+	carrier = node
+	isSlowing = false
+	isCarried = true
